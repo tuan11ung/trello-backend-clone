c1f39da217805bae535ea0f0146c45246ab1a940
tuan11ung <nguyenduytuan11ung@gmail.com>
2025-10-03 23:49:15 +0700

    connect to mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,44 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+/**
+ * ntuanhung82
+ * LvJL9ztKLTiRvtPb
+ */
+
+const MONGODB_URI = 'mongodb+srv://ntuanhung82:LvJL9ztKLTiRvtPb@cluster0-tuan11ungdev.vyjlqrn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-Tuan11ungDev'
+const DATABASE_NAME = 'trello-fullstack'
+
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+//Khoi tao 1 obj trelloDatabaseInstance ban dau la null (vi chua ket noi)
+let trelloDatabaseInstance = null;
+
+//Khoi tao 1 obj mongoClientInstance de connect toi MongoDB
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  //Luu y: serverApi co tu mongodb v5.0.0 tro len, co the khong can dung no, con neu dung la chung ta se chi dinh 1 Stable API version cua mongoDB
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+//connect toi MongoDB va tra ve obj database neu thanh cong
+export const CONNECT_DB = async () => {
+  //Goi ket noi toi MongoDB Atlas voi URI da khai bao trong than cuar mongoClientInstance
+  await mongoClientInstance.connect();
+
+  //Ket noi thanh cong thi lay ra DB theo ten va gan nguoc no vao bien toan cuc trelloDatabaseInstance
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+
+}
+
+
+//Function GET_DB(ko phai async) co nhiem vu export ra cai Trello Database Instance sau khi da connect thanh cong toi MongoDB de chung ta su dung o nhieu noi khac nhau trong code
+//Luu y: dam bao chi goi GET_DB sau khi da goi CONNECT_DB thanh cong
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to database first!')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -5,29 +5,46 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB } from '~/config/mongodb.js'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' }],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+    res.end('<h1>Hello World!</h1>')
+  })
+
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`3. Hello Trung Quan Dev, Backend server is running successfully at http://${hostname}:${port}/`)
+  })
+}
+
+// (async () => {
+//   try {
+//     console.log('1. Trying to connect to MongoDB...')
+//     await CONNECT_DB()
+//     console.log('2. Connected to MongoDB successfully!')
+//     START_SERVER()
+//   } catch (error) {
+//     console.error(error)
+//     process.exit(1)
+//   }
+// })()
+
+//Chi khi ket noi toi DB thanh cong thi moi Start server
+console.log('1. Trying to connect to MongoDB...')
+CONNECT_DB()
+  .then( () => {
+    console.log('2. Connected to MongoDB successfully!')
+  })
+  .then( () => START_SERVER())
+  .catch( error => {
+    console.error(error)
+    process.exit(1)
+  })
