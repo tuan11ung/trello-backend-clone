ed725ddabe1d96f798b13b9e6b179bb88c91b876
Tuan11ung <nguyenduytuan11ung@gmail.com>
2025-10-14 00:40:27 +0700

    create error handling middleware

--- src/controllers/board.controller.js
+++ src/controllers/board.controller.js
@@ -1,15 +1,22 @@
 import { StatusCodes } from "http-status-codes";
+import ApiError from "~/utils/ApiError";
 
 const createNew = async (req, res, next) => {
     try {
-        console.log('req.body:', req.body)
+        // console.log('req.body:', req.body)
+        // console.log('req.query:', req.query)
+        // console.log('req.params:', req.params)
+        // console.log('req.files:', req.filesy)
+        // console.log('req.cookies:', req.cookies)
+        // console.log('req.jwtDecoded:', req.jwtDecoded)
+ 
+        //Dieu huong du lieu sang tang Service
+
+        // throw new ApiError(StatusCodes.BAD_GATEWAY, 'co loi nha')
+
         res.status(StatusCodes.CREATED).json({ message: 'API creates new board from controller'})
     } catch (error) {
-        console.log(error)
-            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-                errors: error.message
-            })
-        
+        next(error)
     }
 }
 
