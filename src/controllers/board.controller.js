162d11ef761e1cf60872cf400688bdc875763801
Tuan11ung <nguyenduytuan11ung@gmail.com>
2025-10-16 09:46:44 +0700

    create service layer

--- src/controllers/board.controller.js
+++ src/controllers/board.controller.js
@@ -1,20 +1,15 @@
 import { StatusCodes } from "http-status-codes";
+import { boardService } from "~/services/board.service"
 import ApiError from "~/utils/ApiError";
 
 const createNew = async (req, res, next) => {
     try {
-        // console.log('req.body:', req.body)
-        // console.log('req.query:', req.query)
-        // console.log('req.params:', req.params)
-        // console.log('req.files:', req.filesy)
-        // console.log('req.cookies:', req.cookies)
-        // console.log('req.jwtDecoded:', req.jwtDecoded)
- 
         //Dieu huong du lieu sang tang Service
+        const createdBoard = await boardService.creatNew(req.body)
 
         // throw new ApiError(StatusCodes.BAD_GATEWAY, 'co loi nha')
 
-        res.status(StatusCodes.CREATED).json({ message: 'API creates new board from controller'})
+        res.status(StatusCodes.CREATED).json(createdBoard)
     } catch (error) {
         next(error)
     }
